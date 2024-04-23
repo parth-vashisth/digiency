859a399059b2857938205565c73390b7aceb04f5
parth vashisth <shrmaparth123@gmail.com>
2024-04-24 00:32:13 +0530

    responsive half done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,8 @@
+let navicon = document.querySelector(".nav-icon");
+let lgview = document.querySelector(".lg-view");
+navicon.addEventListener("click", () => {
+    lgview.classList.toggle("show");
+});
 
 $(".parentslider").slick({
     dots: false,
